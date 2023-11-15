bc0e3b5b22b98ec88d029903815d00efb3d16a1b
VocanicZ <ampnattahapat@gmail.com>
2023-11-16 05:34:16 +0700

    Update Lobby Warning.

--- src/main/java/net/rainbowcreation/serverExtension/main.java
+++ src/main/java/net/rainbowcreation/serverExtension/main.java
@@ -1,22 +1,16 @@
 package net.rainbowcreation.serverExtension;
 
 import ibxm.Player;
-import net.minecraft.client.audio.Sound;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.MobEffects;
+import net.minecraft.network.play.server.SPacketTitle;
 import net.minecraft.potion.PotionEffect;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.PlayerList;
-import net.minecraft.util.SoundEvent;
 import net.minecraft.util.text.TextComponentString;
-import net.minecraft.world.Teleporter;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldProvider;
-import net.minecraft.world.WorldServer;
-import net.minecraftforge.common.DimensionManager;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -29,6 +23,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Spliterator;
 
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
@@ -62,6 +57,7 @@ public class main {
                 "# |_|  \\_\\__,_|_|_| |_|____/ \\___/ \\_/\\_/  \\_____|_|  \\___|\\__,_|\\__|_|\\___/|_|  |_| #",
                 "#                                                                                    #",
                 "################################################################sever-extension#######");
+        System.out.println("Mode:" +Reference.MODE);
         for (String txt : header) {
             System.out.println(txt);
         }
@@ -72,7 +68,6 @@ public class main {
         }
         for (int j = 1; j <= 10; j++)
             WARNING_TIME_LIST.add(j);
-        System.out.println(WARNING_TIME_LIST);
     }
 
     @SubscribeEvent
@@ -92,46 +87,99 @@ public class main {
                     }
                 }
             }
-            // V only in server
-            /*
-            playerList.sendMessage((ITextComponent) new TextComponentString(TextFormatting.BOLD + "[Clear Lag] " + TextFormatting.RESET + "Cleared " + TextFormatting.RED + amount + TextFormatting.RESET + " items!"));
-            for (EntityPlayerMP player : plist) {
-                player.addExperience(50);
+            if (Reference.MODE.equals("server")) {
+                playerList.sendMessage((ITextComponent) new TextComponentString(TextFormatting.BOLD + "[Clear Lag] " + TextFormatting.RESET + "Cleared " + TextFormatting.RED + amount + TextFormatting.RESET + " items!"));
             }
-            playerList.sendMessage((ITextComponent) new TextComponentString(TextFormatting.BOLD + "[Sorry Gift] " + TextFormatting.RESET + "Sorry for roll back here was " + TextFormatting.GREEN + "50" + TextFormatting.RESET + " exps. you will get this gift every 30 minutes!"));
-            */
-            // A only in server
             timeInTicks = staticTimeInTicks;
-        } // V only in server
-        /*
-        for (int i: WARNING_TIME_LIST) {
-            if (i * 20 == timeInTicks) {
-                long[] lst = Time.secondToMinute(i);
-                TextComponentString text = new TextComponentString(TextFormatting.BOLD + "[Clear Lag] " + TextFormatting.RESET + "Items will be cleared :");
-                if (lst[0] > 0)
-                     text.appendSibling(new TextComponentString(" " + TextFormatting.RED + String.valueOf(lst[0]) + TextFormatting.RESET + " minutes"));
-                if (lst[1] > 0)
-                    text.appendSibling(new TextComponentString(" " + TextFormatting.RED + String.valueOf(lst[1]) + TextFormatting.RESET + " seconds"));
-                text.appendText("!!.");
-                playerList.sendMessage(text);
+        }
+        switch (Reference.MODE) {
+            case ("server"): {
+                for (int i: WARNING_TIME_LIST) {
+                    if (i * 20 == timeInTicks) {
+                        long[] lst = Time.secondToMinute(i);
+                        TextComponentString text = new TextComponentString(TextFormatting.BOLD + "[Clear Lag] " + TextFormatting.RESET + "Items will be cleared :");
+                        if (lst[0] > 0)
+                            text.appendSibling(new TextComponentString(" " + TextFormatting.RED + String.valueOf(lst[0]) + TextFormatting.RESET + " minutes"));
+                        if (lst[1] > 0)
+                            text.appendSibling(new TextComponentString(" " + TextFormatting.RED + String.valueOf(lst[1]) + TextFormatting.RESET + " seconds"));
+                        text.appendText("!!.");
+                        playerList.sendMessage(text);
+                    }
+                }
+                break;
+            }
+            case ("lobby"): {
+                if (timeInTicks%20 != 0)
+                    break;
+                int[] time = Time.getCurrentTime();
+                if (time[0] >= 3 && time[0] < 6) {
+                    TextComponentString text = new TextComponentString("Time remaining ");
+                    if (time[0] < 5)
+                        text.appendSibling(new TextComponentString(TextFormatting.RED + String.valueOf(5 - time[0]) + TextFormatting.RESET + ":"));
+                    String minute = String.valueOf(59 - time[1]);
+                    String second = String.valueOf(59 - time[2]);
+                    if (minute.length() == 1)
+                        minute = "0" + minute;
+                    if (second.length() == 1)
+                        second = "0" + second;
+                    SPacketTitle timeTitle = new SPacketTitle(SPacketTitle.Type.SUBTITLE, text.appendSibling(new TextComponentString(TextFormatting.RED + minute + TextFormatting.RESET + ":" + TextFormatting.RED + second)), 0, 20, 0);
+                    for (EntityPlayerMP playerMP : plist) {
+                        playerMP.connection.sendPacket(Reference.PACKET_MAINTENANCE);
+                        playerMP.connection.sendPacket(timeTitle);
+                    }
+                }
+                else if (time[0] == 6 && time[1] == 0 && time[2] == 0)
+                    playerList.sendMessage(new TextComponentString(TextFormatting.BOLD + "[Maintenance Completed] " + TextFormatting.RESET + "please reconnect the server to enter the portal!"));
+                break;
             }
         }
-         */
-        // A only in server
         if (timeInTicks > 0)
             timeInTicks--;
     }
 
     @SubscribeEvent
     public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
-        double x, y, z;
-        x = -23.5;
-        y = 63.5;
-        z = 0.5;
-        //Teleport.teleport(event.player, 0, x, y, z);
-        event.player.setPositionAndUpdate(x, y, z);
-        //event.player.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 60*20, 4));
-        event.player.sendMessage(new TextComponentString(TextFormatting.BOLD + "[Queue] " + TextFormatting.RESET + "enter the portal to join queue")); // only in lobby
-        //event.player.sendMessage(new TextComponentString(TextFormatting.BOLD + "[Auth] " + TextFormatting.RESET + "please login or register\n/login <password>\nor\n/register <password> <password>"));
+        //V only in looby
+        EntityPlayer player = event.player;
+        switch (Reference.MODE) {
+            case ("lobby"): {
+                double x, y, z;
+                x = -23.5;
+                y = 63.5;
+                z = 0.5;
+                player.setPositionAndUpdate(x, y, z);
+                player.sendMessage(new TextComponentString(TextFormatting.BOLD + "[Queue] " + TextFormatting.RESET + "enter the portal to join queue"));
+                EntityPlayerMP playerMP = (EntityPlayerMP) player;
+                SPacketTitle packetActionBar = new SPacketTitle(SPacketTitle.Type.ACTIONBAR, new TextComponentString(player.getName()), 20,120,20);
+                playerMP.connection.sendPacket(Reference.PACKET_TITLE);
+                playerMP.connection.sendPacket(Reference.PACKET_SUB_TITLE);
+                playerMP.connection.sendPacket(packetActionBar);
+                break;
+            }
+            case ("server"): {
+                player.addPotionEffect(new PotionEffect(MobEffects.ABSORPTION, 60 * 20, 4));
+                player.sendMessage(new TextComponentString(TextFormatting.BOLD + "[Auth] " + TextFormatting.RESET + "please login or register\n/login <password>\nor\n/register <password> <password>"));
+                break;
+            }
+        }
+        /*
+        if (getSkinStorage().getSkin(event.player.func_110124_au()) == SkinStorage.DEFAULT_SKIN)
+            getSkinStorage().setSkin(event.player.func_110124_au(), MojangSkinProvider.getSkin(event.player.func_146103_bH().getName()));
+        applySkin((EntityPlayerMP)event.player, getSkinStorage().getSkin(event.player.func_110124_au()));
+        */
     }
+    /*
+
+    @SubscribeEvent
+    public void onPlayerLeaving(PlayerEvent.PlayerLoggedOutEvent event) {
+        getSkinStorage().removeSkin(event.player.func_110124_au());
+    }
+
+    public static void onClosingServer(FMLServerStoppingEvent ev
+                                       ent) {
+        for (EntityPlayerMP player : server.func_184103_al().func_181057_v())
+            getSkinStorage().removeSkin(player.func_110124_au());
+    }
+
+     */
 }
--- src/main/java/net/rainbowcreation/serverExtension/utils/Time.java
+++ src/main/java/net/rainbowcreation/serverExtension/utils/Time.java
@@ -1,5 +1,9 @@
 package net.rainbowcreation.serverExtension.utils;
 
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
 public class Time {
     public static long[] secondToMinute(long second) {
         long[] lst = new long[2];
@@ -7,5 +11,15 @@ public class Time {
         lst[0] = second/60;
         lst[1] = second%60;
         return (lst);
+    }
+
+    public static int[] getCurrentTime() {
+        int[] lst = new int[3];
+
+        ZonedDateTime time = ZonedDateTime.now(ZoneId.ofOffset("UTC", ZoneOffset.of("+07:00")));
+        lst[0] = time.getHour();
+        lst[1] = time.getMinute();
+        lst[2] = time.getSecond();
+        return lst;
     }
 }
